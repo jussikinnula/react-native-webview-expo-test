9bb8872d144233092171400a065f1d4c7d0462c0
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2020-05-07 10:09:38 +0300

    Install react-native-webview 8.1.1 (recommended version of latest Expo SDK)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <WebView source={{ uri: 'https://reactnative.dev' }} />
     </View>
   );
 }
